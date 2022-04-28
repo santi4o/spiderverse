315630c6914d088507d0fd0968b51039128fd556
Aldo S <aldo11santiago@gmail.com>
2022-04-27 21:01:43 -0500

    testing Spiderman object attributes

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,16 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+const Spiderman = require('../app/spiderman');
+
+describe("Unit tests for Spiderman class", () => {
+    test('1) Create a Spiderman instance', () => {
+        // Aquí escribimos el código que queremos usar tal cual
+        // Quiero poder instanciar un objeto Spiderman con esta información
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony");
+
+        // Validamos que este código funcione de la forma esperada
+        expect(andrewGarfield.name).toBe("Spiderman Sony");
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe("Andrew Garfield");
+        expect(andrewGarfield.movies).toBe(2);
+        expect(andrewGarfield.studio).toBe("Sony");
     });
 })
